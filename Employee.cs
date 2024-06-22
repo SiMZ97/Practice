5cd2a4073d55ee382f132cfd7b3f29f4da576720
SiMZ <simz@Siavashs-MBP.lan>
2024-06-22 11:22:24 -0700

    Initial commit

---
+++ Employee.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Practice;
+
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDate;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        public Employee(string first, string last, string em, DateTime bd): this(first, last, em, bd, 0)
+        {
+
+        }
+
+        public Employee(string first, string last, string em, DateTime bd, double rate)
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+            birthDate = bd;
+            hourlyRate = rate;
+        }
+
+        public void PerformWork()
+        {
+            PerformWork(minimalHoursWorkedUnit);
+        }
+
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+
+            Console.WriteLine($"{firstName} {lastName} has worked for" +
+                              $"{numberOfHoursWorked} hour(s)!");
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;        
+        }
+
+        public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+        {
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }  
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}"); 
+            return bonus; 
+        }
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has received ${wage}" + 
+                              $"for {numberOfHoursWorked} hours of work!");
+
+            if (resetHours)
+            {
+                numberOfHoursWorked = 0;
+            }
+            return wage;    
+        }
+
+        public void DisplayEmployeeDetails()
+        {   
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}" +
+                              $"\nEmail: \t{email}\nBirth date: \t{birthDate}" +
+                              $"\nHourly rate: \t{hourlyRate}\nNumber of hours worked: \t{numberOfHoursWorked}" +
+                              $"\nWage: \t{wage}");
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Practice;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+bethany.PerformWork(25);
+
+int minimumBonus = 100;
+int bonusTax = 0;
+int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, ref bonusTax);
+Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
+
